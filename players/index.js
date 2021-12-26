09e649acc1da3c45690073a1ed84724360184df6
Natalya <natasha.masalckova@yandex.ru>
2021-12-26 20:31:45 +0300

    Homework-8

--- players/index.js
+++ players/index.js
@@ -6,39 +6,35 @@ const IMG = {
     SUBZERO: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
 };
 
-export const player1 = {
+class Player {
+    constructor(props) {
+        this.player = props.player;
+        this.name = props.name;
+        this.hp = props.hp;
+        this.img = props.img;
+    }
+    changeHP = function changeHP(randomNumber) {
+        this.hp -= randomNumber;
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
+    }
+    elHP = () => document.querySelector(`.player${this.player} .life`);
+    renderHP = () =>(this.elHP()).style.width = `${this.hp}%`;
+}
+
+export const player1 = new Player ({
     player: 1,
     name: 'SCORPION',
     hp: 100,
     img: IMG['SCORPION'],
     weapon: ['Sweet Voice'],
-    changeHP,
-    elHP,
-    renderHP,
-};
+})
 
-export const player2 = {
+export const player2 = new Player ({
     player: 2,
     name: 'SUBZERO',
     hp: 100,
     img: IMG['SUBZERO'],
     weapon: ['Round videos in Telegram'],
-    changeHP,
-    elHP,
-    renderHP,
-};
-
-function changeHP(randomNumber) {
-    this.hp -= randomNumber;
-    if (this.hp <= 0) {
-        this.hp = 0;
-    }
-}
-
-function elHP() {
-    return document.querySelector(`.player${this.player} .life`);
-}
-
-function renderHP() {
-    (this.elHP()).style.width = `${this.hp}%`;
-}
+})
