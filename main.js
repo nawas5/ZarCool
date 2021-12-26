3397cda9199b08e712d2250c5a1d5a7a37cccf4c
Natalya <natasha.masalckova@yandex.ru>
2021-12-26 22:33:11 +0300

    Homework-9

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
 import {gameAffect} from './game/logic.js';
-import {player1, player2} from './players/index.js';
 import {generateLogs} from './logs/index.js';
 import {renderPlayer, showResult} from './game/layout.js';
+
+
+
+// let player1;
+// let player2;
+//
+// class Game {
+//     getPlayers = async () => {
+//         return fetch('https://reactmarathon-api.herokuapp.com/api/mk/players').then(res => res.json());
+//     }
+//
+//     getEnemy = async () => {
+//         return fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose').then(res => res.json());
+//     }
+//
+//     getFight = async () => {
+//         return fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 hit,
+//                 defence,
+//             })
+//         });
+//     }
+//
+//     start = async () => {
+//         const players = await this.getPlayers();
+//         const enemy = await this.getEnemy();
+//         const p1 = players[getRandom(players.length) - 1];
+//         const p2 = players[getRandom(enemy.length) - 1];
+//         player1 = new Player ({
+//             ...p1,
+//             player: 1,
+//             rootSelector: 'arenas',
+//         });
+//         player2 = new Player ({
+//             ...p2,
+//             player: 2,
+//             rootSelector: 'arenas',
+//         });
+//     }
+// }
+//
+// const game = new Game();
+// game.start();
 
 const init = () => {
     const $arenas = document.querySelector('.arenas');
